f65b1133e873a2c0991e50e8e40d0d1e06df7292
KALINE DA SILVA MARQUES <a95054686@rn.senac.local>
2023-09-19 16:36:54 -0300

    Exemplo 2

---
+++ aula2-atividade.py
@@ -0,0 +1,10 @@
+#calcula o 0 de uma função afim: ax+b=0
+
+print("ax+b=0")
+a = float(input("Insira o valor de a: "))
+b = float(input("Insira o valor de b: "))
+
+x = -b/a
+
+print(f'O zero da função {a}x + {b} = 0 é {x}.')
+
