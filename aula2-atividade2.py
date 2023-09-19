b80e1b99b9624427b4fcf55e58366e401b4fc0cb
Kaline Marques <134382161+kalinemarques@users.noreply.github.com>
2023-09-19 16:51:31 -0300

    Update aula2-atividade2.py
    Correção da mensagem de saída.

--- aula2-atividade2.py
+++ aula2-atividade2.py
@@ -10,5 +10,5 @@ delta = b**2 - (4*a*c)
 x1 = (-b + delta**0.5)/(2*a)
 x2 = (-b - delta**0.5)/(2*a)
 
-print(f'As raízes da equação {a}x² + {b}x + c = 0 são {x1} e {x2}')
+print(f'As raízes da equação {a}x² + {b}x + {c} = 0 são {x1} e {x2}')
 
