59b76996b52ddf0a4a73cc430ee58d1be275e2f4
Kaline Marques <134382161+kalinemarques@users.noreply.github.com>
2023-10-11 13:54:39 -0300

    Soma de todos os números de 1 a n

--- uc5/Exemplo6.py
+++ uc5/Exemplo6.py
@@ -12,3 +12,20 @@ for i in range(0,n+1):
 print(f'\nA soma de todos os pares entre 1 e {n} é {soma}')
 
 del(n, soma)
+
+#Soma de todos os números de 1 até n usando while
+
+n = int(input("Insira um número: "))
+
+soma = 0
+
+print(f'\nA soma de todos os pares entre 1 e {n} é ', end="")
+
+while n>=0:
+    if n%2==0:
+        soma += n
+    n -= 1
+
+print(f'{soma}')
+
+del(n, soma)
