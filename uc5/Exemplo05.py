11139e422a6ade0ccfcf4c075f3c2e37ab9c0719
Kaline Marques <kaline_marques@outlook.com>
2023-10-11 15:01:52 -0300

    cálcula a quantidade de trigo

---
+++ uc5/Exemplo05.py
@@ -0,0 +1,10 @@
+#Cálcula a quantidade de trigo de forma exponencial
+
+i = 1
+quantidadeTrigo = 1
+
+while(i<=64):
+    i += 1
+    quantidadeTrigo *= 2
+
+print(f'A quantidade de trigo é {quantidadeTrigo}')
