aa46b39f2bbf037a76a021d77440a86c323f6722
Kaline Marques <kaline_marques@outlook.com>
2023-10-20 16:02:43 -0300

    Exemplo de polimorfismo - dispositivos conectados

---
+++ uc5/Exemplo18.py
@@ -0,0 +1,45 @@
+class dispositivoEletronico():
+    def acao(self):
+        pass
+    def produto(self):
+        pass
+
+class Celular(dispositivoEletronico):
+    def acao(self):
+        return 'carregando'
+    def produto(self):
+        return 'Samsung Galaxy A14'
+    
+class FoneDeOuvido(dispositivoEletronico):
+    def acao(self):
+        return 'conectado'
+    def produto(self):
+        return 'Galaxy Buds2 '
+
+class Computador(dispositivoEletronico):
+    def acao(self):
+        return 'sendo atualizado...'
+    def produto(self):
+        return 'Notebook Dell Inspiron 15'
+    
+class Impressora(dispositivoEletronico):
+    def acao(self):
+        return 'imprimindo'
+    def produto(self):
+        return 'Epson l3250'
+    
+class Relogio(dispositivoEletronico):
+    def acao(self):
+        return 'conectado'
+    def produto(self):
+        return 'Galaxy Watch6'
+    
+def oQueEstaAcontecendo(dispositivos):
+    for dispositivo in dispositivos:
+        print(f'O dispositivo {dispositivo.produto()} está {dispositivo.acao()}.')
+
+
+dispositivos = [Celular(), FoneDeOuvido(), Computador(), Relogio()]
+oQueEstaAcontecendo(dispositivos)
+
+        
